78c925b35fe8c2c0497590bbdc758294f2ba2664
Stefano <stefanofabi96@gmail.com>
2021-05-28 03:14:27 -0300

    Realizar tablero de la administración interna

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $user_administrator = new User(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@community',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $user_administrator->saveOrFail();
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"> {{ trans('home.dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +15,62 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="row">
+                        <div class="col">
+                            <div style="text-align: center;">
+                                <a class="nav-link" style="color: black" href="">
+                                    <h1>
+                                        <i style="font-size: 8vw" class="fas fa-users"></i>
+                                    </h1>
+
+                                    <br />
+
+                                    {{ trans('home.administrators') }}
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col" style="text-align: center;">
+                            <a class="nav-link" style="color: black" href="#">
+                                <h1>
+                                    <i style="font-size: 8vw" class="fas fa-key"></i>
+                                </h1>
+
+                                <br />
+                                {{ trans('home.ranks') }}
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div style="text-align: center;">
+                                <a class="nav-link" style="color: black" href="">
+                                    <h1>
+                                        <i style="font-size: 8vw" class="fas fa-server"></i>
+                                    </h1>
+
+                                    <br />
+
+                                    {{ trans('home.servers') }}
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div style="text-align: center;">
+                                <a class="nav-link" style="color: black" href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <h1>
+                                        <i style="font-size: 8vw" class="fas fa-sign-out-alt"></i>
+                                    </h1>
+
+                                    <br />
+
+                                    {{ trans('auth.logout') }}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
