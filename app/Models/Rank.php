61705a35dfbfdaa3f8ab8db8867f746bc76786f6
Stefano <stefanofabi96@gmail.com>
2021-07-18 21:11:37 -0300

    Add activity logs trait in the rank model

--- app/Models/Rank.php
+++ app/Models/Rank.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Rank extends Model
 {
